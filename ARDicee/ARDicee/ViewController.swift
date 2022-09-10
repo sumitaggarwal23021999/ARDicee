2179acaaf4c47fbd2d38b3c5d2d3c4c509c120ad
Sumit Aggarwal <sumit.aggarwal@algoworks.com>
2022-09-11 00:26:41 +0530

    added code for showing a static 3D dice in air, then have coded to detect a plane and show image once horizontal plane get detected.

--- ARDicee/ARDicee/ViewController.swift
+++ ARDicee/ARDicee/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -12,7 +12,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         //MARK: Craeting of a cube
 //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
@@ -26,21 +26,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        sceneView.automaticallyUpdatesLighting = true
         
         //MARK: Craeting of a Sphere(Moon)
-        let sphere = SCNSphere(radius: 0.2)
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpeg")
-        sphere.materials = [material]
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 1, z: -0.5)
-        node.geometry = sphere
-        sceneView.scene.rootNode.addChildNode(node)
-        sceneView.automaticallyUpdatesLighting = true
+//        let sphere = SCNSphere(radius: 0.2)
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpeg")
+//        sphere.materials = [material]
+//        let node = SCNNode()
+//        node.position = SCNVector3(x: 0, y: 1, z: -0.5)
+//        node.geometry = sphere
+//        sceneView.scene.rootNode.addChildNode(node)
+//        sceneView.automaticallyUpdatesLighting = true
         
-        // Create a new scene
+        
+        
+        //MARK: Create a new scene and showing of ship
 //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
 //
 //        // Set the scene to the view
 //        sceneView.scene = scene
+        
+        //MARK: Create static 3D dice in air
+//        sceneView.automaticallyUpdatesLighting = true
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//            sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
+        
+        //MARK: Detecting horizontal plane and showing 3D dice onto it
+        sceneView.automaticallyUpdatesLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -50,6 +63,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        let configuration = ARWorldTrackingConfiguration()
         let configuration = ARWorldTrackingConfiguration()
         debugPrint(AROrientationTrackingConfiguration.isSupported)
+        
+        //MARK: Line for plane detection
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -60,5 +76,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let planeAnchor = anchor as? ARPlaneAnchor {
+            debugPrint("Plane detected")
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
 }
